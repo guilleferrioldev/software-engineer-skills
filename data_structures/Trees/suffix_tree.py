dd9e0593f1aef0d50162ade92785fa11719b8237
guilleferrioldev <guillermoferrioldev@gmail.com>
2024-01-05 09:02:59 -0500

    implemented suffix tree

--- data_structures/Trees/suffix_tree.py
+++ data_structures/Trees/suffix_tree.py
@@ -1,82 +1,75 @@
+from typing import Self, Optional, List, Set
+
 class Node:
-    def __init__(self, start, end, suffix_link=None):
-        self.start = start 
-        self.end = end 
-        self.suffix_link = suffix_link 
-        self.children = {}  
+    """Node implementation"""
+    def __init__(self, substring: str = "", children: Optional["Node"] = None) -> None:
+        self.substring = substring
+        self.children = children or []
 
 class SuffixTree:
-    def __init__(self, string):
-        self.root = Node(-1, -1)  
-        self.string = string 
-        self.build()  
+    """Suffix Tree implementation"""
+    def __init__(self, text: str) -> None:
+        self.text = text + "$"
+        self.nodes = [Node()]
+        
+        for i in range(len(self.text)):
+            self.add_suffix(self.text[i:])
 
-    def build(self):
-        active_node = self.root
-        active_edge = 0
-        active_length = 0
-        remainder = 0
+        self._suffixes = []
+        self._collect_suffixes(0, "", self._suffixes)
 
-        for index in range(len(self.string)):
-            remainder += 1
-            last_created_node = None
+    def add_suffix(self, suffix: str) -> None:
+        """Ukkonen's Suffix Tree Algorithm"""
+        node_index = 0
+        i = 0
+        while i < len(suffix):
+            current_char = suffix[i]
+            child_index = 0
+            while True:
+                children = self.nodes[node_index].children
+                if child_index == len(children):
+                    # There is no matching child, the rest of the suffix becomes a new node
+                    new_node_index = len(self.nodes)
+                    self.nodes.append(Node(suffix[i:], []))
+                    self.nodes[node_index].children.append(new_node_index)
+                    return
+                next_node_index = children[child_index]
+                if self.nodes[next_node_index].substring[0] == current_char:
+                    break
+                child_index = child_index + 1
+            # find prefix of remaining suffix in common with child
+            common_substring = self.nodes[next_node_index].substring
+            j = 0
+            while j < len(common_substring):
+                if suffix[i + j] != common_substring[j]:
+                    # Split the node next_node_index
+                    new_node_index = next_node_index
+                    # New node for the common part
+                    next_node_index = len(self.nodes)
+                    self.nodes.append(Node(common_substring[:j], [new_node_index]))
+                    self.nodes[new_node_index].substring = common_substring[j:] # The old node loses the common part
+                    self.nodes[node_index].children[child_index] = next_node_index
+                    break  # Continuar bajando en el árbol
+                j = j + 1
+            i = i + j  # Move beyond the common part
+            node_index = next_node_index  # Continue down the tree
 
-            while remainder > 0:
-                if active_length == 0:
-                    active_edge = index
+    def _collect_suffixes(self, node_index: int, current_suffix: str, suffixes: List) -> None:
+        """Method to return all suffixes"""
+        children = self.nodes[node_index].children
+        if len(children) == 0:
+            suffix = current_suffix + self.nodes[node_index].substring
+            if suffix != "$":
+                suffixes.append(suffix.replace("$", ""))
+        else:
+            for child in children:
+                self._collect_suffixes(child, current_suffix + self.nodes[node_index].substring, suffixes)
+                
+    def __eq__(self, other: Self) -> Optional[Set]: 
+        """Method to return all suffixes in common whith another Suffix Tree"""
+        if isinstance(other, SuffixTree):
+            result = set(self._suffixes).intersection(set(other._suffixes)) 
+            return result if result else None
 
-                if self.string[index] not in active_node.children:
-                    leaf_node = Node(index, len(self.string)-1)
-                    active_node.children[self.string[index]] = leaf_node
-                    if last_created_node is not None:
-                        last_created_node.suffix_link = active_node
-                        last_created_node = None
-
-                else:
-                    next_node = active_node.children[self.string[active_edge]]
-                    next_length = next_node.end - next_node.start + 1
-                    if active_length >= next_length:
-                        active_edge += next_length
-                        active_length -= next_length
-                        active_node = next_node
-                        continue
-
-                    if self.string[next_node.start + active_length] == self.string[index]:
-                        active_length += 1
-                        if last_created_node is not None and active_node != self.root:
-                            last_created_node.suffix_link = active_node
-                            last_created_node = None
-                        break
-
-                    split_node = Node(next_node.start, next_node.start + active_length - 1)
-                    split_node.children[self.string[index]] = Node(index, len(self.string)-1)
-                    split_node.children[self.string[next_node.start + active_length]] = next_node
-                    next_node.start += active_length
-                    active_node.children[self.string[active_edge]] = split_node
-
-                    if last_created_node is not None:
-                        last_created_node.suffix_link = split_node
-                    last_created_node = split_node
-
-                remainder -= 1
-                if active_node == self.root and active_length > 0:
-                    active_length -= 1
-                    active_edge = index - remainder + 1
-                elif active_node != self.root:
-                    active_node = active_node.suffix_link
-                    
-    def get_all_suffixes(self, node, suffixes):
-        if node.start != -1 and node.end != -1:
-            suffixes.append(self.string[node.start : node.end + 1])
-        for child in node.children.values():
-            self.get_all_suffixes(child, suffixes)
-
-    def __repr__(self):
-        suffixes = []
-        self.get_all_suffixes(self.root, suffixes)
-        return f"Suffices: {suffixes}"
-                    
-
-input_string = "banana"
-suffix_tree = SuffixTree(input_string)
-print(suffix_tree)
+    def __repr__(self) -> str:
+        return f"{self._suffixes}"
